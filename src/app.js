994173d795ead3aff28d32ebf19b6f0e6ba4eb06
AnyiRojas <anyicamilamendivelsorojas@gmail.com>
2024-11-23 14:06:53 -0500

    despliegue

--- src/app.js
+++ src/app.js
@@ -38,6 +38,8 @@ app.use(express.json());
 app.use(fileUpload({
   createParentPath: true,
 }));
+
+// Ruta para servir archivos estáticos
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Endpoint para listar imágenes
@@ -45,13 +47,15 @@ app.get('/api/images/producto', (req, res) => {
   const dirPath = path.join(__dirname, 'uploads/img/producto');
   fs.readdir(dirPath, (err, files) => {
     if (err) {
-      return res.status(500).send('Error al leer el directorio.');
+      console.error('Error al leer el directorio:', err);  // Log detallado del error
+      return res.status(500).send('Error al leer el directorio de imágenes.');
     }
     const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
     res.json(images);
   });
 });
 
+// Rutas para servir archivos estáticos con serve-index
 app.use('/uploads/img/pedido', serveIndex(path.join(__dirname, 'uploads/img/pedido'), { icons: true }));
 app.use('/uploads/img/fecha_especial', serveIndex(path.join(__dirname, 'uploads/img/fecha_especial'), { icons: true }));
 app.use('/uploads/img/tipo_flor', serveIndex(path.join(__dirname, 'uploads/img/tipo_flor'), { icons: true }));
@@ -62,10 +66,15 @@ app.use(usuarioRoutes, productoRoutes, opcionadicionalRoutes, pedidoRoutes, pedi
 
 // Middleware para manejo de errores
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error('Error en el servidor:', err.stack);
   res.status(500).json({ message: 'Algo salió mal. Intenta nuevamente más tarde.' });
 });
 
 app.use('/api', AuthRouter);
+
+// Manejo de errores 404 (si no se encuentra ninguna ruta)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+});
 
 export default app;
